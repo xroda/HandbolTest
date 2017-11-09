fb294b146f1f71ba86cf60e6938b27e9cf1fb50e
Xavier Roda <xavier.roda@gmail.com>
2017-11-09 13:36:20 +0100

    First upload

--- Handbol/Resources/Resource.Designer.cs
+++ Handbol/Resources/Resource.Designer.cs
@@ -44,8 +44,35 @@ namespace Handbol
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050002
+			public const int btnPart1 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int btnPart2 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int btnPart3 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int btnPart4 = 2131034117;
+			
+			// aapt resource value: 0x7f050007
+			public const int btnTimerPause = 2131034119;
+			
+			// aapt resource value: 0x7f050006
+			public const int btnTimerPlay = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int btnTimerReset = 2131034120;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int gridLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050009
+			public const int myButton = 2131034121;
+			
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
 			
 			static Id()
 			{
@@ -92,11 +119,14 @@ namespace Handbol
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f040002
+			public const int app_name = 2130968578;
 			
 			// aapt resource value: 0x7f040000
 			public const int hello = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int txtPart = 2130968577;
 			
 			static String()
 			{
